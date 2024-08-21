d8544ed1d954854ef772edb9bd5213a6466c87b9
cfredberg <53453343+cfredberg@users.noreply.github.com>
2024-08-20 21:38:45 -0700

    changed paths with thought process on what to do
    the new plan:
    have the cameras stream to ip addresses, and open them with a web server running on the laptop end
    
    the server would then also get keyboard input, get the motor commands, send them over to the jetson using sockets, and have the jetson open them and set them accordingly.

--- main/main_jetson.py
+++
@@ -1,9 +0,0 @@
-import server.app
-import master_process_util as mutil
-
-import multiprocessing
-
-if __name__ == '__main__':
-    server_process = mutil.create_process(server.app.process, (), "server")
-
-    server_process.join()
--- main/master_process_util.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-
-def create_process(target, args, name):
-    process_name = f"{name}_process"
-    p = multiprocessing.Process(target=target, args=args, name=process_name)
-    p.daemon = True
-    p.start()
-    print(f"Created Process: {process_name}")
-    return p
